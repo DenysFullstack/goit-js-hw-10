bf42200e0c4cf1b53e85e3f4ec7381f1ff8d6780
DenysFullstack <allcarinae@gmail.com>
2023-09-15 15:14:17 +0300

    fourth commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,3 @@
-
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
@@ -33,15 +32,3 @@ export function fetchCatByBreed(catId) {
     return response.json();
   });
 }
-
-// const CAT_API_INFO = `https://api.thecatapi.com/v1/images/search?breed_ids=${e.target.value}&api_key=live_1NuflkvHe5UEH3sqKTw8yu41EpLIlEmEzyL3cypmYINhQESTegEfCuGLo138Hhfs`;
-// console.log(CAT_API_INFO);
-// return fetch(
-//   `https://api.thecatapi.com/v1/images/search?breed_ids=${e.target.value}&api_key=live_1NuflkvHe5UEH3sqKTw8yu41EpLIlEmEzyL3cypmYINhQESTegEfCuGLo138Hhfs`
-// )
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import axios from 'axios';
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css'
 import Notiflix from 'notiflix';
-
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_1NuflkvHe5UEH3sqKTw8yu41EpLIlEmEzyL3cypmYINhQESTegEfCuGLo138Hhfs';
 
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
@@ -59,18 +55,8 @@ function onSearch(e) {
         `❌ Oops! Something went wrong! Try reloading the page!`
       );
     })
-    // .finally(() => {
-    //   loader.classList.add('visually-hidden');
-    // });
 }
-// new SlimSelect({
-//   select: select,
-//   events: {
-//     beforeClose: () => {
-//       console.log('before close')
-//     },
-//   },
-// })
+
 function renderCatBreedInfo(arr) {
   loader.classList.add('visually-hidden')
   catInfo.classList.remove('visually-hidden')
@@ -89,76 +75,3 @@ function renderCatBreedInfo(arr) {
     })
     .join('');
 }
-
-
-
-
-// console.log(cats);
-// new SlimSelect({
-//   select: select,
-//   data: cats.map(cat=>({text:cat.name,value:cat.id}))
-//   })
-
-
-
-
-// new SlimSelect({
-//   select: '.breed-select',
-//   data: cats
-// })
-
-// fetchCatByBreed()
-//   .then(arr => {
-//     console.log(arr);
-//     const markup = arr
-//       .map(({ url, breeds }) => {
-//         return `
-//       <div>
-//         <img src="${url}" alt="${breeds[0].name}" width=300>
-//         <h2>${breeds[0].name}</h2>
-//         <p>${breeds[0].description}</p>
-//         <p>Temperament: ${breeds[0].temperament}</з>
-//       </div>
-//     `;
-//       })
-//       .join('');
-//     catInfo.innerHTML = markup;
-//   })
-//   .catch(err => console.log(err));
-
-
-
-
-
-// function fetchBreeds(){
-//   const BASE_URL = 'https://api.thecatapi.com/v1'
-//   const API_KEY = 'live_1NuflkvHe5UEH3sqKTw8yu41EpLIlEmEzyL3cypmYINhQESTegEfCuGLo138Hhfs'
-//   return axios.get(
-//     'https://api.thecatapi.com/v1/breeds?api_key=live_1NuflkvHe5UEH3sqKTw8yu41EpLIlEmEzyL3cypmYINhQESTegEfCuGLo138Hhfs')
-// }
-
-// function onSearch(e) {
-//   console.log(e.target.value);
-//   const catId = e.target.value
-//   fetchCatByBreed(catId)
-//     .then(catBreedInfo => {
-//       console.log(catBreedInfo);
-//       catInfo.innerHTML = renderCatBreedInfo(catBreedInfo);
-//     })
-//     .catch(err => console.log(err));
-// }
-
-// function renderCatBreedInfo(arr) {
-//   return arr
-//     .map(({ url, breeds }) => {
-//       return `
-//       <div>
-//         <img src="${url}" alt="${breeds[0].name}" width=300>
-//         <h2>${breeds[0].name}</h2>
-//         <p>${breeds[0].description}</p>
-//         <p>Temperament: ${breeds[0].temperament}</з>
-//       </div>
-//     `;
-//     })
-//     .join('');
-// }
